ff6fd346c092cb6c6c044d7064010aeddd1c7b71
Cynthia Tristán <cyntrist@ucm.es>
2025-07-04 05:07:32 +0200

    hola

--- Scripts/PreProduction/BoardCreator.gd
+++ Scripts/PreProduction/BoardCreator.gd
@@ -2,14 +2,58 @@
 class_name BoardCreator
 extends Node
 
+@export var width: int = 10
+@export var depth: int = 10
+@export var height: int = 8
+@export var pos: Vector2i
+var _oldPos: Vector2i
+var tiles = {}
+
+var tileViewPrefab = preload("res://Prefabs/Tile.tscn")
+var tileSelectionIndicatorPrefab = preload("res://Prefabs/TileIndicator.tscn")
+var marker
+
 func Clear():
-	print("Clear Pressed")
+	for key in tiles:
+		tiles[key].free()
+	tiles.clear()
+
+func _GetOrCreate(p: Vector2i):
+	if tiles.has(p):
+		return tiles[p]
+	
+	var t: Tile = _Create()
+	t.Load(p, 0)
+	tiles[p] = t
+	
+	return t
+
+func _Create():
+	var instance = tileViewPrefab.instantiate()
+	add_child(instance)
+	return instance
+
+func _GrowSingle(p: Vector2i):
+	var t: Tile = _GetOrCreate(p)
+	if t.height < height:
+		t.Grow()
+		_UpdateMarker()
+func _ShrinkSingle(p: Vector2i):
+	if not tiles.has(p):
+		return
+	
+	var t: Tile = tiles[p]
+	t.Shrink()
+	_UpdateMarker()
+	
+	if t.height <= 0:
+		tiles.erase(p)
+		t.free()
 
 func Grow():
-	print("Grow Pressed")
-
+	_GrowSingle(pos)
 func Shrink():
-	print("Shrink Pressed")
+	_ShrinkSingle(pos)
 
 func GrowArea():
 	print("GrowArea Pressed")
@@ -29,11 +73,25 @@ func SaveJSON():
 func LoadJSON():
 	print("LoadJSON Pressed")
 
-# Called when the node enters the scene tree for the first time.
+
+
+
 func _ready():
-	pass # Replace with function body.
+	marker = tileSelectionIndicatorPrefab.instantiate()
+	add_child(marker)
+	
+	pos = Vector2i(0,0)
+	_oldPos = pos
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if pos != _oldPos:
+		_oldPos = pos
+		_UpdateMarker()
+
+
+func _UpdateMarker():
+	if tiles.has(pos):
+		var t: Tile = tiles[pos]
+		marker.position = t.center()
+	else:
+		marker.position = Vector3(pos.x, 0, pos.y)
