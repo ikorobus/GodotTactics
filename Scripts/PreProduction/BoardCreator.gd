06fc090ea8edf1ff6e924c6f3390108463b2fb60
Cynthia Tristán <cyntrist@ucm.es>
2025-07-04 05:23:40 +0200

    falta comprobar guardar y cargar mapas

--- Scripts/PreProduction/BoardCreator.gd
+++ Scripts/PreProduction/BoardCreator.gd
@@ -12,6 +12,10 @@ var tiles = {}
 var tileViewPrefab = preload("res://Prefabs/Tile.tscn")
 var tileSelectionIndicatorPrefab = preload("res://Prefabs/TileIndicator.tscn")
 var marker
+var _random = RandomNumberGenerator.new()
+var savePath = "res://Data/Levels/"
+@export var fileName = "defaultMap.txt"
+
 
 func Clear():
 	for key in tiles:
@@ -55,23 +59,115 @@ func Grow():
 func Shrink():
 	_ShrinkSingle(pos)
 
-func GrowArea():
-	print("GrowArea Pressed")
 
+func _GrowRect(rect: Rect2i):
+	for y in range(rect.position.y,rect.end.y):
+		for x in range(rect.position.x,rect.end.x):
+			var p = Vector2i(x,y)
+			_GrowSingle(p)
+func _ShrinkRect(rect: Rect2i):
+	for y in range(rect.position.y,rect.end.y):
+		for x in range(rect.position.x,rect.end.x):
+			var p = Vector2i(x,y)
+			_ShrinkSingle(p)
+
+func GrowArea():
+	var r: Rect2i = _RandomRect()
+	_GrowRect(r)
+	
 func ShrinkArea():
-	print("ShrinkArea Pressed")
+	var r: Rect2i = _RandomRect()
+	_ShrinkRect(r)
 
 func Save():
-	print("Save Pressed")
+	var saveFile = savePath + fileName
+	var save_game = FileAccess.open(saveFile, FileAccess.WRITE)
+	var version = 1
+	var size = tiles.size()
+	
+	save_game.store_8(version)
+	save_game.store_16(size)
+
+	for key in tiles:
+		save_game.store_8(key.x)
+		save_game.store_8(key.y)
+		save_game.store_8(tiles[key].height)
+		
+	save_game.close()
 
 func Load():
-	print("Load Pressed")
+	Clear()
+	
+	var saveFile = savePath + fileName
+	if not FileAccess.file_exists(saveFile):
+		return # Error! We don't have a save to load.
+		
+	var save_game = FileAccess.open(saveFile, FileAccess.READ)
+	var version = save_game.get_8()
+	var size = save_game.get_16()
+	
+	for i in range(size):
+		var save_x = save_game.get_8()
+		var save_z = save_game.get_8()
+		var save_height = save_game.get_8()
+		
+		var t: Tile = _Create()
+		t.Load(Vector2i(save_x, save_z) , save_height)
+		tiles[Vector2i(t.pos.x,t.pos.y)] = t	
+	
+	save_game.close()
+	_UpdateMarker()
 
 func SaveJSON():
-	print("SaveJSON Pressed")
+	var main_dict = {
+		"version": "1.0.0",
+		"tiles": []
+	}
+		
+	for key in tiles:
+		var save_dict = {
+			"pos_x" : tiles[key].pos.x,
+			"pos_z" : tiles[key].pos.y,
+			"height" : tiles[key].height
+			}
+		main_dict["tiles"].append(save_dict)
+
+	#var saveFile = savePath + fileName
+	#var save_game = FileAccess.open(saveFile, FileAccess.WRITE)
+	var save_game = FileAccess.open("res://Data/Levels/savegame.json", FileAccess.WRITE)
+	
+	var json_string = JSON.stringify(main_dict, "\t", false)
+	save_game.store_line(json_string)
 
 func LoadJSON():
-	print("LoadJSON Pressed")
+	Clear()
+
+	#var saveFile = savePath + fileName
+	#if not FileAccess.file_exists(saveFile):
+	if not FileAccess.file_exists("res://Data/Levels/savegame.json"):
+		return # Error! We don't have a save to load.
+	
+	#var save_game = FileAccess.open(saveFile, FileAccess.READ)	
+	var save_game = FileAccess.open("res://Data/Levels/savegame.json", FileAccess.READ)	
+
+	var json_text = save_game.get_as_text()	
+	var json = JSON.new()
+	var parse_result = json.parse(json_text)
+
+	if parse_result != OK:
+		print("Error %s reading json file." % parse_result)
+		return
+		
+	var data = {}
+	data = json.get_data()
+
+	for mtile in data["tiles"]:
+		var t: Tile = _Create()
+		t.Load(Vector2(mtile["pos_x"], mtile["pos_z"]) , mtile["height"])
+		tiles[Vector2i(t.pos.x,t.pos.y)] = t
+	
+	save_game.close()
+	_UpdateMarker()
 
 
 
@@ -82,6 +178,8 @@ func _ready():
 	
 	pos = Vector2i(0,0)
 	_oldPos = pos
+	
+	_random.randomize()
 
 func _process(delta):
 	if pos != _oldPos:
@@ -95,3 +193,10 @@ func _UpdateMarker():
 		marker.position = t.center()
 	else:
 		marker.position = Vector3(pos.x, 0, pos.y)
+			
+func _RandomRect():
+	var x = _random.randi_range(0, width - 1)
+	var y = _random.randi_range(0, depth - 1)
+	var w = _random.randi_range(1, width - x)
+	var h = _random.randi_range(1, depth - y)
+	return Rect2i ( x, y, w, h )
